03cb77b8bcea20d527515d5fd0da71f4a8a95b01
nhabitonho <buitienthanhlong0604@gmail.com>
2021-03-25 23:25:39 +0700

    first commit

---
+++ src/demo.java
@@ -0,0 +1,51 @@
+public class demo {
+    public static void main(String[] args) {
+//        1 : comment code
+//        ctrl + "/" "**"
+//        2 : kiểu dữ liệu
+//         nhom kieu du lieu nguyen thuy : không hỗ trợ phương thức xử lý
+//         nhom kieu du lieu doi tuong : hõ trợ phương thức xử lý
+//         int, boolean, double, float, char...
+//        String,Integer, Boolean, Double, Float, CharSequence
+/*        3 : khai báo biến
+
+          nháy đơn truyền kí tự, nháy kép truyền chuỗi
+                */
+        String name = "Bùi Tiến Thành Long";
+        int age = 20;
+        float s1 = 1.5f;
+        double s2 = 1.5;
+        char s3 = 'a';
+        /*
+        4 : Toán tử
+        a, b đc gọi là toán hạng
+        Toán tử : +, - , *, /
+        Biểu thức : phải có giá trị trả về
+         */
+//        int a = 5;
+//        a= a +1;
+//        a+=1;
+//        a++;
+//        ++a;
+//        int b = a++;
+//        //int c = a+b;
+//        // số : ++, --,
+//        System.out.println(b);
+//        System.out.println(a);
+
+
+
+        System.out.println("Hello World!!!!");
+    }
+    // 1 : tạo thư mục .git (làm lần đầu)
+    // git init
+
+    //2 : xem sự thay đổi
+    // git status
+
+    //3 : lưu trữ thay đổi
+    // git add.
+
+    //4. lưu trữ vào local repo
+    // git commit -m "tên mesage"
+}
