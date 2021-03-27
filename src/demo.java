941856a664dfde3bc4dbbca4c0f47e2fd74a8657
nhabitonho <buitienthanhlong0604@gmail.com>
2021-03-27 20:15:41 +0700

    buoi3

--- src/demo.java
+++ src/demo.java
@@ -1,5 +1,6 @@
 public class demo {
     public static void main(String[] args) {
+        //System.out.println("Hello World!!!!");
 //        1 : comment code
 //        ctrl + "/" "**"
 //        2 : kiểu dữ liệu
@@ -34,18 +35,34 @@ public class demo {
 //        System.out.println(a);
 
 
+        // 1 : tạo thư mục .git (làm lần đầu)
+        // git init
 
-        System.out.println("Hello World!!!!");
+        //2 : xem sự thay đổi
+        // git status
+
+        //3 : lưu trữ thay đổi
+        // git add.
+
+        //4. lưu trữ vào local repo
+        // git commit -m "tên message"
+    /*
+    5 : cau dieu kien (if else)
+    {} : scope (pham vi)
+     */
+        int a = 5;
+        int b = 10;
+        int ketqua = 0;
+
+//        if (a>b) ketqua = 1;
+//        else if (a==b) ketqua = 0;
+//        else ketqua = -1;
+//        Toán tử ba ngôi
+//        ketqua = a>b ? 1 :a<b ? -1: 0;
+        if (a < b) {
+            return;
+        }
+        System.out.println("Next");
+
     }
-    // 1 : tạo thư mục .git (làm lần đầu)
-    // git init
-
-    //2 : xem sự thay đổi
-    // git status
-
-    //3 : lưu trữ thay đổi
-    // git add.
-
-    //4. lưu trữ vào local repo
-    // git commit -m "tên message"
 }
