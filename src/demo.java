d7bf0d77a541e605cc21c22fda827c2961cb1fac
nhabitonho <buitienthanhlong0604@gmail.com>
2021-03-27 20:28:00 +0700

    switch case

--- src/demo.java
+++ src/demo.java
@@ -35,34 +35,45 @@ public class demo {
 //        System.out.println(a);
 
 
-        // 1 : tạo thư mục .git (làm lần đầu)
-        // git init
-
-        //2 : xem sự thay đổi
-        // git status
-
-        //3 : lưu trữ thay đổi
-        // git add.
-
-        //4. lưu trữ vào local repo
-        // git commit -m "tên message"
+//         1 : tạo thư mục .git (làm lần đầu)
+//         git init
+//
+//        2 : xem sự thay đổi
+//         git status
+//
+//        3 : lưu trữ thay đổi
+//         git add.
+//
+//        4. lưu trữ vào local repo
+//         git commit -m "tên message"
     /*
     5 : cau dieu kien (if else)
     {} : scope (pham vi)
      */
-        int a = 5;
-        int b = 10;
-        int ketqua = 0;
-
+//        int a = 5;
+//        int b = 10;
+//        int ketqua = 0;
+//
 //        if (a>b) ketqua = 1;
 //        else if (a==b) ketqua = 0;
 //        else ketqua = -1;
 //        Toán tử ba ngôi
 //        ketqua = a>b ? 1 :a<b ? -1: 0;
-        if (a < b) {
-            return;
+//        if (a < b) {
+//            return;
+//        }
+//        System.out.println("Next");
+//        Lỗi Runtime
+        int thang= 5;
+        switch (thang){
+            case 1 : System.out.println("Quí 1");
+                break;
+            case 2 : System.out.println("Quí 2");
+                break;
+            case 3 : System.out.println("Quí 3");
+                break;
+            default: System.out.println("Không nằm trong quí");
         }
-        System.out.println("Next");
-
+        System.out.println("Thoát khỏi switch");
     }
 }
